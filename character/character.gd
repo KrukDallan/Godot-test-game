62e2866b09c8966e308904acfa0476804cc06d38
KrukDallan <99502048+KrukDallan@users.noreply.github.com>
2024-01-28 16:49:06 +0100

    ooooOooOoooOoOoO

--- character/character.gd
+++ character/character.gd
@@ -3,7 +3,8 @@ extends RigidBody3D
 var _input := Vector3.ZERO
 # builds up velocity
 var _incremental_speed : float = 1.0
-var dash_factor : float = 1.0
+var dash_factor : float = 2.0
+var shooting_threshold : float = 0.0
 
 var mouse_sensitivity := 0.001
 var twist_input := 0.0
@@ -12,11 +13,16 @@ var pitch_input := 0.0
 @onready var twist_pivot := $TwistPivot
 @onready var pitch_pivot := $TwistPivot/PitchPivot
 @onready var raycast := $TwistPivot/PitchPivot/RayCast3D
+@onready var groundcast := $GroundCast
 
-# load ball
+@export var jump_impulse = 50
+# The downward acceleration when in the air, in meters per second squared.
+@export var fall_acceleration = 9.81
+@export var fall_addendum = 0.0
+
+# Load ball
 var ball = preload("res://objects/ball.tscn")
 var shooted : bool = false
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,12 +30,22 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	shooted = false
+func _physics_process(delta: float) -> void:
+	shooting_threshold += delta
+	if !groundcast.is_colliding():
+		var downward_force_y = - fall_acceleration*delta - fall_addendum
+		
+		apply_central_force(Vector3(0,downward_force_y,0))
+		fall_addendum += delta*50
+	else:
+		fall_addendum = 0.0
+		
+
 	var input := Vector3.ZERO
 	input.x = Input.get_axis("ui_left", "ui_right")
 	input.z = Input.get_axis("ui_up", "ui_down")
 	_input = twist_pivot.basis * input * 15.0 * delta * _incremental_speed * dash_factor
+	
 	var collision = move_and_collide(_input)
 	if collision != null:
 		if collision.get_collider().is_in_group("Tree"):
@@ -42,21 +58,25 @@ func _process(delta: float) -> void:
 		if _incremental_speed > 2.0:
 			_incremental_speed = 2.0
 
-
-
-	
-	if Input.is_action_just_pressed("ui_cancel"):
-		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+			#position.y = position.y - (fall_acceleration * delta)
+			
+	if Input.is_action_pressed("shoot") and shooting_threshold > 5*delta:
+		shooting_threshold = 0.0
+		var instanced = ball.instantiate()
+		instanced.position = raycast.global_position
+		instanced.transform.basis = raycast.global_transform.basis
+		get_tree().get_root().add_child(instanced)
 	
 	twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp($TwistPivot/PitchPivot.rotation.x, -0.4, 0.6)
+	pitch_pivot.rotation.x = clamp($TwistPivot/PitchPivot.rotation.x, -1.0, 1.0)
 	
 	twist_input = 0.0
 	pitch_input = 0.0
+	
+	if Input.is_key_pressed(KEY_SPACE):
+		apply_central_impulse(Vector3(0,3,0))
 
 
 
@@ -65,18 +85,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			twist_input = -event.relative.x * mouse_sensitivity
 			pitch_input = - event.relative.y * mouse_sensitivity
-
-	elif Input.is_action_pressed("shoot") and shooted == false:
-		shooted = true
-		var instanced = ball.instantiate()
-		instanced.position = raycast.global_position
-		instanced.transform.basis = raycast.global_transform.basis
-		get_tree().get_root().add_child(instanced)
 	
 	elif Input.is_action_pressed("dash"):
 		dash_factor = 2.0
 	else:
 		dash_factor = 1.0
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 			
 
